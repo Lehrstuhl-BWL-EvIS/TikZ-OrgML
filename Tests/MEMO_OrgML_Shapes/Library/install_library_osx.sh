543bd814382d2f2d789f4f821a9eaaba6c892405
Fabian Schneider <fabian@fabianschneider.org>
2016-02-16 16:46:48 +0100

    Parallelausführung ist jetzt für mehr als 2 Prozesse möglich.

--- Tests/MEMO_OrgML_Shapes/Library/install_library_osx.sh
+++ Tests/MEMO_OrgML_Shapes/Library/install_library_osx.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Erzeugen des Symlinks
-echo "Erzeuge Symlink"
-sudo ln -s /Users/fabi/Dropbox/Studium/Fernstudium/Masterthesis/Code/Tests/MEMO_OrgML_Shapes/Library/tikzlibrarymemoorgml.code.tex /usr/local/texlive/2015/texmf-dist/tex/generic/pgf/frontendlayer/tikz/libraries/tikzlibrarymemoorgml.code.tex
+# Kopieren der Dateien
+echo "Kopiere Dateien"
+sudo cp /Users/fabi/Dropbox/Studium/Fernstudium/Masterthesis/Code/Tests/MEMO_OrgML_Shapes/Library/tikzlibrarymemoorgml.code.tex /usr/local/texlive/2015/texmf-dist/tex/generic/pgf/frontendlayer/tikz/libraries/tikzlibrarymemoorgml.code.tex
+sudo cp /Users/fabi/Dropbox/Studium/Fernstudium/Masterthesis/Code/Tests/MEMO_OrgML_Shapes/Library/tikzlibrarymemoorgmlshapes.code.tex /usr/local/texlive/2015/texmf-dist/tex/generic/pgf/frontendlayer/tikz/libraries/tikzlibrarymemoorgmlshapes.code.tex
 
 # Update texmf-Tree
 echo "Update texmf-Tree"
